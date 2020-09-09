47575ed7377da1762eb885bf50798d1c79f76929
Parth Shah <parthshah@parths-mbp.lan>
2020-09-08 19:54:32 -0700

    adding build

---
+++ build/precache-manifest.092566ca7d9f7bdd90cf07aa08a637a7.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e70edcb938ab02772b1999772da2f0b6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "07e45871d0049883f75d",
+    "url": "/static/css/main.ba56d751.chunk.css"
+  },
+  {
+    "revision": "535c0cfe8209a3f63775",
+    "url": "/static/js/2.1db8f424.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/static/js/2.1db8f424.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07e45871d0049883f75d",
+    "url": "/static/js/main.1938edcb.chunk.js"
+  },
+  {
+    "revision": "d38f5dae242020185caf",
+    "url": "/static/js/runtime-main.8c912ffd.js"
+  },
+  {
+    "revision": "fdce6cf43176b0fd55c63f4dec6703bb",
+    "url": "/static/media/elizabeth.fdce6cf4.png"
+  },
+  {
+    "revision": "ab811c3351567128ec2a7b0eb30de13b",
+    "url": "/static/media/lisa.ab811c33.png"
+  },
+  {
+    "revision": "211e7149ccbe114e30ae5d2e76b889e4",
+    "url": "/static/media/logo.211e7149.svg"
+  },
+  {
+    "revision": "96d6da807c1e45de3df22bde657c6f18",
+    "url": "/static/media/parth.96d6da80.png"
+  },
+  {
+    "revision": "6962efce8a4d32f81e4caea5e2529697",
+    "url": "/static/media/star.6962efce.svg"
+  },
+  {
+    "revision": "0e6a142b1544bac499d6941b2ec66bd7",
+    "url": "/static/media/sun.0e6a142b.svg"
+  }
+]);
